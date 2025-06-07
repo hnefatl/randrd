9dec9f2ac47341898272cdb59e8b24409e0031ff
Keith Collister <hnefatl@gmail.com>
2025-06-07 18:52:56 +0100

    Barebones daemon.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+
+use anyhow::{self, Context};
+use clap::Parser;
+use pidfile;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(long, default_value = "/var/run/randrd.pid")]
+    pid_file: PathBuf,
+}
+
+fn main() -> Result<(), anyhow::Error> {
+    let args = Args::parse();
+    let _pid_file = pidfile::PidFile::new(args.pid_file).context("Opening PID lockfile")?;
+    Ok(())
 }
