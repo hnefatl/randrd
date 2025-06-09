7ef2e8deb51ee038513f9a23e49fc979a7138927
Keith Collister <hnefatl@gmail.com>
2025-06-09 23:16:22 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use core::f64;
-use std::{collections::HashMap, path::PathBuf};
+use std::{collections::HashMap, ops::Deref, path::PathBuf};
 
-use anyhow::{self, Context, bail};
+use anyhow::{self, Context};
 use clap::Parser;
 use serde::{Deserialize, Serialize};
 
@@ -20,13 +20,21 @@ struct Config {
     // Keying on name might need adjusting in the future since there's no guarantee that monitor names are unique between my computers.
     monitors: HashMap<String, MonitorSpec>,
 }
+// Desired config for a monitor.
 #[derive(Debug, Clone, Deserialize, Serialize)]
 struct MonitorSpec {
     width: u32,
     height: u32,
-    // Refresh rate may not match exactly, closest wins.
-    refresh_rate: Option<f64>,
+    refresh_rate: Option<f64>,  // Refresh rate may not match exactly, closest wins.
+
+    #[serde(default)]
+    primary: bool,
+    #[serde(default)]
+    rotation: xrandr::Rotation,
+    x: u32,
+    y: u32,
 }
+
 impl MonitorSpec {
     const REFRESH_RATE_TOLERANCE: f64 = 1.0;
 
@@ -49,45 +57,67 @@ fn get_edid(output: &xrandr::Output) -> anyhow::Result<edid::EDID> {
     }
 }
 
+enum MonitorResult {
+    Ok(),
+    Skipped(String),
+}
+
+fn process_monitor(
+    xhandle: &mut xrandr::XHandle,
+    config: &Config,
+    monitor: &xrandr::Monitor,
+    modes: &Vec<xrandr::Mode>,
+) -> anyhow::Result<MonitorResult> {
+    macro_rules! skip {
+        ($($arg:tt)*) => { return Ok(MonitorResult::Skipped(format!($($arg)*))); };
+    }
+
+    let Some(monitor_config) = config.monitors.get(&monitor.name) else {
+        skip!("unconfigured");
+    };
+    let [output] = &monitor.outputs[..] else {
+        skip!(
+            "has >1 output: {:?}",
+            monitor.outputs.iter().map(|o| &o.name)
+        );
+    };
+
+    let compatible_modes = monitor_config.get_compatible_modes(&modes);
+    if compatible_modes.is_empty() {
+        skip!("unable to find compatible modes");
+    };
+    if let Some(current_mode) = output
+        .current_mode
+        .and_then(|id| compatible_modes.iter().find(|m| m.xid == id))
+    {
+        skip!("already assigned a compatible mode: {}", current_mode.name);
+    }
+    if let [compatible_mode] = compatible_modes[..] {
+        xhandle.set_mode(output, compatible_mode)?;
+        xhandle.set_rotation(output, monitor_config.rotation.into())?;
+        xhandle.set_position(output, relation, relative_output)
+        return Ok(MonitorResult::Ok());
+    }
+    skip!(
+        "found >1 compatible modes, can't choose: {:?}",
+        compatible_modes
+    );
+}
+
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     let _pid_file = pidfile::PidFile::new(args.pid_file).context("Opening PID lockfile")?;
 
     let mut xhandle = xrandr::XHandle::open()?;
     let modes = xrandr::ScreenResources::new(&mut xhandle)?.modes();
-    for monitor in xhandle.monitors()? {
-        let Some(monitor_config) = args.config.monitors.get(&monitor.name) else {
-            println!("Skipping unconfigured monitor: {}", monitor.name);
-            continue;
-        };
-        let [output] = &monitor.outputs[..] else {
-            println!("Skipping monitor with >1 output: {}", monitor.name);
-            continue;
-        };
 
-        let compatible_modes = monitor_config.get_compatible_modes(&modes);
-        if compatible_modes.is_empty() {
-            println!(
-                "Unable to find compatible modes for monitor {}: ",
-                monitor.name
-            );
-            continue;
-        };
-        if output
-            .current_mode
-            .is_some_and(|id| compatible_modes.iter().any(|m| m.xid == id))
-        {
-            println!(
-                "Skipping monitor already assigned a compatible mode: {}",
-                monitor.name
-            );
-            continue;
-        }
-        if let [compatible_mode] = compatible_modes[..] {
-            println!("Single compatible mode, updating: {:?}", compatible_mode);
-            xhandle.set_mode(output, compatible_mode)?;
-        } else {
-            println!("Expected exactly one compatible mode for monitor {}, found: {:?}", monitor.name, compatible_modes);
+    for monitor in xhandle.monitors()? {
+        match process_monitor(&mut xhandle, &args.config, &monitor, &modes) {
+            Ok(MonitorResult::Ok()) => {}
+            Ok(MonitorResult::Skipped(reason)) => {
+                println!("Skipped monitor {}: {}", monitor.name, reason)
+            }
+            Err(e) => println!("{}", e),
         }
     }
     Ok(())
